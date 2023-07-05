d04015393c3a6ea0bca17f74e01741dd947488c5
MissVeronica <veronica@tranza.se>
2023-07-05 12:08:35 +0200

    Update um-ratings-members-directory.php

--- um-ratings-members-directory.php
+++ um-ratings-members-directory.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Ratings in Members Directory
  * Description:     Extension to Ultimate Member for adding Ratings to the Members Directory Page.
- * Version:         1.1.0
+ * Version:         1.2.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v3 or later
@@ -36,42 +36,52 @@ class UM_Ratings_Members_Directory {
 
         if ( empty( $form_ids ) || in_array( $directory_data['form_id'], $form_ids )) {
 
-            $rating_keys = array_map( 'trim', array_map( 'sanitize_text_field', explode( ',', UM()->options()->get( 'um_ratings_members_directory' ))));           
-            $list_order = array();
-            $list_title = array();
-            
-            foreach( $rating_keys as $rating_key ) {
+            $roles = UM()->options()->get( 'um_ratings_members_directory_roles' );
 
-                $stars = um_user( $rating_key );
-                if ( ! empty( $stars )) {
+            if ( is_array( $roles )) {
+                $roles = array_map( 'sanitize_text_field', $roles );
+                $prio_role =  UM()->roles()->get_priority_user_role( $user_id );
+            }
 
-                    $rating_field = UM()->builtin()->get_a_field( $rating_key );
+            if ( empty( $roles ) || in_array( $prio_role, $roles )) {
 
-                    if ( ! empty( $rating_field ) && $rating_field['type'] == 'rating' ) {
-                        if ( $rating_field['public'] == 1 || ( $rating_field['public'] == 2 && is_user_logged_in())) {
+                $rating_keys = array_map( 'trim', array_map( 'sanitize_text_field', explode( ',', UM()->options()->get( 'um_ratings_members_directory' ))));
+                $list_order = array();
+                $list_title = array();
 
-                            $list_order[$rating_key] = $stars;
-                            $list_title[$rating_key] = isset( $rating_field['title'] ) ? $rating_field['title'] : $rating_field['label'];
+                foreach( $rating_keys as $rating_key ) {
+
+                    $stars = um_user( $rating_key );
+                    if ( ! empty( $stars )) {
+
+                        $rating_field = UM()->builtin()->get_a_field( $rating_key );
+
+                        if ( ! empty( $rating_field ) && $rating_field['type'] == 'rating' ) {
+                            if ( $rating_field['public'] == 1 || ( $rating_field['public'] == 2 && is_user_logged_in())) {
+
+                                $list_order[$rating_key] = $stars;
+                                $list_title[$rating_key] = isset( $rating_field['title'] ) ? $rating_field['title'] : $rating_field['label'];
+                            }
                         }
                     }
                 }
-            }
 
-            $sorting = trim( sanitize_text_field( UM()->options()->get( 'um_ratings_members_directory_sorting' )));
+                $sorting = trim( sanitize_text_field( UM()->options()->get( 'um_ratings_members_directory_sorting' )));
 
-            if ( $sorting == 'descending' ) arsort( $list_order );
-            if ( $sorting == 'ascending' ) asort( $list_order );
+                if ( $sorting == 'descending' ) arsort( $list_order );
+                if ( $sorting == 'ascending' ) asort( $list_order );
 
-            foreach( $list_order as $key => $stars ) {
+                foreach( $list_order as $key => $stars ) {
 
-                $data_array['ratings'] .= '<div>' . $list_title[$key] . '</div><div class="um-rating-members">';
-                $data_array['ratings_line'] .= '<div>' . $list_title[$key] . '&nbsp;';
-                for ( $i = 1; $i <= $stars; $i++ ) { 
-                    $data_array['ratings'] .= '<i class="star-on-png" title="' . $stars . '"></i>';
-                    $data_array['ratings_line'] .= '<i class="star-on-png" title="' . $stars . '"></i>';
+                    $data_array['ratings'] .= '<div>' . $list_title[$key] . '</div><div class="um-rating-members">';
+                    $data_array['ratings_line'] .= '<div>' . $list_title[$key] . '&nbsp;';
+                    for ( $i = 1; $i <= $stars; $i++ ) { 
+                        $data_array['ratings'] .= '<i class="star-on-png" title="' . $stars . '"></i>';
+                        $data_array['ratings_line'] .= '<i class="star-on-png" title="' . $stars . '"></i>';
+                    }
+                    $data_array['ratings'] .= '</div>';
+                    $data_array['ratings_line'] .=  '</div>';
                 }
-                $data_array['ratings'] .= '</div>';
-                $data_array['ratings_line'] .=  '</div>';
             }
         }
 
@@ -85,14 +95,14 @@ class UM_Ratings_Members_Directory {
             'type'          => 'text',
             'label'         => __( 'Ratings Members Directory - Meta Keys', 'ultimate-member' ),
             'tooltip'       => __( 'Name of the rating meta keys comma separated.', 'ultimate-member' )
-            );
-            
+        );
+
         $settings_structure['misc']['fields'][] = array(
             'id'            => 'um_ratings_members_directory_form_ids',
             'type'          => 'text',
             'label'         => __( 'Ratings Members Directory - Form Ids', 'ultimate-member' ),
-            'tooltip'       => __( 'Form Ids comma separated.', 'ultimate-member' )
-            );
+            'tooltip'       => __( 'Form Ids comma separated. Blank all Members Directories.', 'ultimate-member' )
+        );
 
         $settings_structure['misc']['fields'][] = array(
             'id'            => 'um_ratings_members_directory_sorting',
@@ -103,7 +113,17 @@ class UM_Ratings_Members_Directory {
                                         'descending' => __( 'Descending', 'ultimate-member' ) ),
             'label'         => __( 'Ratings Members Directory - Sorting stars', 'ultimate-member' ),
             'tooltip'       => __( 'Select No sorting (meta_key order), Ascending or Descending number of stars.', 'ultimate-member' )
-            );
+        );
+
+        $settings_structure['misc']['fields'][] = array(
+            'id'          => 'um_ratings_members_directory_roles',
+            'type'        => 'select',
+            'multi'       => true,
+            'options'     => UM()->roles()->get_roles(),
+            'label'       => __( 'Ratings Members Directory - Select Roles for viewing', 'ultimate-member' ),
+            'tooltip'     => __( 'Leave empty if you want to display ratings for all logged in users', 'ultimate-member' ),
+            'size'        => 'small',
+        );
 
         return $settings_structure;
     }
